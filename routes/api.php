931317f4f3627d3a1cdb15e28841264041f0dd50
Sőrés Gábor <gabor.sores86@gmail.com>
2024-11-27 08:53:47 +0100

    feature: put all controller resources to api routes

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\CompanyAddressController;
+use App\Http\Controllers\CompanyOwnerController;
+use App\Http\Controllers\CompanyEmployeeController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('company', CompanyController::class);
+Route::apiResource('company-owner', CompanyOwnerController::class);
+Route::apiResource('company-employee', CompanyEmployeeController::class);
+Route::apiResource('company-address', CompanyAddressController::class);

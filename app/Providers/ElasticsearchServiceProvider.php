6e7c689b5f66353090a98b6295554cce6ddb0fd1
Sőrés Gábor <gabor.sores86@gmail.com>
2024-12-02 07:21:53 +0100

    feature: set authentication in elastic provider

--- app/Providers/ElasticsearchServiceProvider.php
+++ app/Providers/ElasticsearchServiceProvider.php
@@ -2,17 +2,24 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Elastic\Elasticsearch\Client;
 use Elastic\Elasticsearch\ClientBuilder;
+use Illuminate\Support\ServiceProvider;
 
 class ElasticsearchServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->singleton('elasticsearch', function () {
-            return ClientBuilder::create()
-                ->setHosts(config('elasticsearch.hosts'))
-                ->build();
+        $this->app->singleton(Client::class, function () {
+            $client = ClientBuilder::create()
+                ->setHosts(config('elasticsearch.hosts'));
+            $elasticUser = env('ELASTICSEARCH_USER');
+            $elasticPassword = env('ELASTICSEARCH_PASS');
+            if ($elasticUser && $elasticPassword) {
+                $client->setBasicAuthentication($elasticUser, $elasticPassword);
+            }
+
+            return $client->build();
         });
     }
 }

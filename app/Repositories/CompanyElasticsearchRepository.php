826e3cfacd8d42a60b8be8c23fd8cbc69b5db359
Sőrés Gábor <gabor.sores86@gmail.com>
2024-12-02 08:55:37 +0100

    feature: add a repository with interface to handle the company's elastic methods

--- app/Repositories/CompanyElasticsearchRepository.php
+++ app/Repositories/CompanyElasticsearchRepository.php
@@ -7,23 +7,73 @@ use App\Repositories\Interfaces\CompanyElasticsearchRepositoryInterface;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Exception;
+use Elastic\Elasticsearch\Client as ElasticClient;
+use Http\Promise\Promise;
+use \Elastic\Elasticsearch\Response\Elasticsearch as ElasticsearchResponse;
+use Illuminate\Support\Collection;
 
 class CompanyElasticsearchRepository implements CompanyElasticsearchRepositoryInterface
 {
 
-    public function search(array $filters): LengthAwarePaginator
+    protected ElasticClient $elasticsearch;
+
+    public function __construct(ElasticClient $elasticsearch)
+    {
+        $this->elasticsearch = $elasticsearch;
+    }
+
+    public function search(array $filters): ElasticsearchResponse|Promise
     {
         // TODO: Implement search() method.
     }
 
     public function index(array $filters = []): LengthAwarePaginator
     {
-        // TODO: Implement index() method.
+
+        $perPage = $filters['itemsPerPage'] ?? 10;
+        $page = $filters['page'] ?? 1;
+        $from = ($page - 1) * $perPage;
+
+        $response = $this->elasticsearch->search([
+            'index' => 'companies',
+            'body' => [
+                'from' => $from,
+                'size' => $perPage,
+                'query' => [
+                    'bool' => [
+                        'must' => [
+                            ['match_all' => (object)[]]
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+
+        $resultArray = $response->asArray();
+        $companies = collect($resultArray['hits']['hits'])->map(function ($hit) {
+            return $hit['_source'];
+        });
+
+        $total = $resultArray['hits']['total']['value'];
+
+        return new LengthAwarePaginator(
+            $companies,
+            $total,
+            $perPage,
+            $page,
+            [
+                'path' => request()->url(),
+                'query' => request()->query()
+            ]
+        );
     }
 
-    public function show(int $id): Company
+    public function show(int $id): ElasticsearchResponse|Promise
     {
-        // TODO: Implement show() method.
+        return $this->elasticsearch->get([
+            'index' => 'companies',
+            'id' => $id
+        ]);
     }
 
     public function store(array $data): Company
@@ -36,13 +86,37 @@ class CompanyElasticsearchRepository implements CompanyElasticsearchRepositoryIn
         // TODO: Implement update() method.
     }
 
-    public function destroy(int $id): bool|null
+    public function destroy(int $id): ElasticsearchResponse|Promise
     {
-        // TODO: Implement destroy() method.
+        return $this->elasticsearch->delete([
+            'index' => 'companies',
+            'id' => $id
+        ]);
     }
 
-    public function checkExistsByRegistrationNumber(string $registrationNumber): bool
+    public function createIndexIfNeeded(): void
     {
-        // TODO: Implement checkExistsByRegistrationNumber() method.
+        $indexExists = $this->elasticsearch->indices()->exists([
+            'index' => 'companies',
+        ]);
+
+        if ($indexExists->getStatusCode() !== 200) {
+            $this->elasticsearch->indices()->create([
+                'index' => 'companies',
+                'body'  => [
+                    'settings' => [
+                        'number_of_shards' => 1,
+                        'number_of_replicas' => 0
+                    ],
+                    'mappings' => [
+                        'properties' => [
+                            'name' => ['type' => 'text'],
+                            'location' => ['type' => 'text'],
+                        ]
+                    ]
+                ]
+            ]);
+        }
     }
+
 }
--- app/Repositories/Interfaces/CompanyElasticsearchRepositoryInterface.php
+++ app/Repositories/Interfaces/CompanyElasticsearchRepositoryInterface.php
@@ -3,8 +3,19 @@
 namespace App\Repositories\Interfaces;
 
 use Illuminate\Pagination\LengthAwarePaginator;
+use Http\Promise\Promise;
+use \Elastic\Elasticsearch\Response\Elasticsearch as ElasticsearchResponse;
+use Illuminate\Support\Collection;
 
-interface CompanyElasticsearchRepositoryInterface extends CompanyRepositoryInterface
+interface CompanyElasticsearchRepositoryInterface
 {
-    public function search(array $filters): LengthAwarePaginator;
+    public function index(array $filters = []): LengthAwarePaginator;
+
+    public function search(array $filters): ElasticsearchResponse|Promise;
+
+    public function show(int $id): ElasticsearchResponse|Promise;
+
+    public function destroy(int $id): ElasticsearchResponse|Promise;
+
+    public function createIndexIfNeeded(): void;
 }
